70c381bdeefa960c854d41fb9e2e2a48808e1ab2
viji-dev <vijisubu2314@gmail.com>
2026-06-30 12:07:27 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chatapp-two-hazel.vercel.app"
+    ],
     credentials: true,
   })
 );
