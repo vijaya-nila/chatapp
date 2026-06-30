9bde6c1b02951446f88d082e8c14ddd858e59ddc
viji-dev <vijisubu2314@gmail.com>
2026-06-30 12:06:39 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chatapp-two-hazel.vercel.app"
+    ],
+    credentials: true,
   },
 });
 
